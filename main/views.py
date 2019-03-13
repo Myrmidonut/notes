b454f5ec043b1cc325cf743be18a45df501ceed0
Frederik Funke <frederik-funke@gmx.de>
2019-03-14 00:23:27 +0100

    json response

--- main/views.py
+++ main/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 from .models import List, Entry
 
 def index(request):
-  data = "Main View"
   lists = List.objects.all()
   entries = Entry.objects.all()
 
-  return render(request, "main/index.html", {"data": data, "lists": lists, "entries": entries})
+  return render(request, "main/index.html", {"lists": lists, "entries": entries})
+
+def json(request):
+  lists = list(List.objects.values())
+  entries = list(Entry.objects.values())
+
+  data = {
+    "data": {
+      "lists": lists,
+      "entries": entries,
+    }
+  }
+
+  return JsonResponse(data, safe=False)
