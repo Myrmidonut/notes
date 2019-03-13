5c67c77fab50d816fcbabdfb159183edcc410da6
Frederik Funke <frederik-funke@gmx.de>
2019-03-13 19:41:40 +0100

    list entry models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Entry, List
 
-# Register your models here.
+class ListAdmin(admin.ModelAdmin):
+  list_display = ("title", "collapsed", "archived")
+
+class EntryAdmin(admin.ModelAdmin):
+  list_display = ("header", "text", "amount", "done")
+
+admin.site.register(List, ListAdmin)
+admin.site.register(Entry, EntryAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class List(models.Model):
+  title = models.CharField(max_length=255)
+  collapsed = models.BooleanField(default=False)
+  archived = models.BooleanField(default=False)
+
+class Entry(models.Model):
+  header = models.ForeignKey(List, on_delete=models.CASCADE, null=True)
+  text = models.CharField(max_length=255)
+  amount = models.FloatField(null=True)
+  done = models.BooleanField(default=False)
