a36dc77600a54161447301529b25357e7e5b1a8b
Frederik Funke <frederik-funke@gmx.de>
2019-03-13 20:33:33 +0100

    show lists with entries, basic buttons

--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import List, Entry
 
 def index(request):
   data = "Main View"
+  lists = List.objects.all()
+  entries = Entry.objects.all()
 
-  return render(request, "main/index.html", {"data": data})
+  return render(request, "main/index.html", {"data": data, "lists": lists, "entries": entries})
