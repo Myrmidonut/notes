15ea71446f001a9caff5379eb725e6e35749449f
Frederik Funke <frederik-funke@gmx.de>
2019-03-16 13:54:03 +0100

    refactor into components, token from cookie

--- static/vue.js
+++ static/vue.js
@@ -1,11 +1,53 @@
-const app = new Vue({
-  delimiters: ["[[", "]]"],
-  el: "#app",
-  data: {
-    "message": "my Vue",
-    "lists": undefined
+Vue.component("archive", {
+  data: function() {
+    return {
+      "test": undefined
+    }
   },
+
+  mounted: function() {
+    this.getCookie('csrftoken')
+    this.getData()
+  },
+
   methods: {
+    
+  }
+})
+
+Vue.component("list", {
+  data: function() {
+    return {
+      "token": undefined,
+      "lists": undefined
+    }
+  },
+
+  mounted: function() {
+    this.getCookie('csrftoken')
+    this.getData()
+  },
+
+  methods: {
+    getCookie: function(name) {
+      let cookieValue = null;
+
+      if (document.cookie && document.cookie != '') {
+        let cookies = document.cookie.split(';');
+
+        for (let i = 0; i < cookies.length; i++) {
+          let cookie = cookies[i].trim();
+
+          if (cookie.substring(0, name.length + 1) == (name + '=')) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            break;
+          }
+        }
+      }
+
+      this.token = cookieValue;
+    },
+
     getData: function() {
       fetch("/get_all")
       .then(res => res.json())
@@ -18,10 +60,10 @@ const app = new Vue({
 
     newList: function(e) {
       let headers = new Headers()
-      headers.append("X-CSRFToken", e.target[0].value)
+      headers.append("X-CSRFToken", this.token)
 
       let body = new FormData()
-      body.append("title", e.target[1].value)
+      body.append("title", e.target[0].value)
 
       fetch("/new_list/", {
         method: "post",
@@ -38,11 +80,11 @@ const app = new Vue({
       const id = e.target.parentElement.id
 
       let headers = new Headers()
-      headers.append("X-CSRFToken", e.target[0].value)
+      headers.append("X-CSRFToken", this.token)
 
       let body = new FormData()
       body.append("id", id)
-      body.append("title", e.target[1].value)
+      body.append("title", e.target[0].value)
 
       fetch("/update_list/", {
         method: "post",
@@ -62,7 +104,7 @@ const app = new Vue({
       this.lists[id].archived ? archived = false : archived = true
 
       let headers = new Headers()
-      headers.append("X-CSRFToken", e.target.parentElement.childNodes[0].value)
+      headers.append("X-CSRFToken", this.token)
 
       let body = new FormData()
       body.append("id", id)
@@ -86,7 +128,7 @@ const app = new Vue({
       this.lists[id].collapsed ? collapsed = false : collapsed = true
 
       let headers = new Headers()
-      headers.append("X-CSRFToken", e.target.parentElement.childNodes[0].value)
+      headers.append("X-CSRFToken", this.token)
 
       let body = new FormData()
       body.append("id", id)
@@ -109,11 +151,11 @@ const app = new Vue({
       const id = e.target.parentElement.id
 
       let headers = new Headers()
-      headers.append("X-CSRFToken", e.target[0].value)
+      headers.append("X-CSRFToken", this.token)
 
       let body = new FormData()
-      body.append("text", e.target[1].value)
-      body.append("amount", e.target[2].value)
+      body.append("text", e.target[0].value)
+      body.append("amount", e.target[1].value)
       body.append("id", id)
 
       fetch("/new_entry/", {
@@ -131,7 +173,7 @@ const app = new Vue({
       const id = e.target.parentElement.id
 
       let headers = new Headers()
-      headers.append("X-CSRFToken", e.target.parentElement.childNodes[0].value)
+      headers.append("X-CSRFToken", this.token)
 
       let body = new FormData()
       body.append("id", id)
@@ -150,13 +192,12 @@ const app = new Vue({
     checkEntry: function(e) {
       const id = e.target.parentElement.id
       const listId = e.target.parentElement.parentElement.parentElement.id
-
       let checked
 
       this.lists[listId].entries[id].done ? checked = false : checked = true
 
       let headers = new Headers()
-      headers.append("X-CSRFToken", e.target.parentElement.childNodes[0].value)
+      headers.append("X-CSRFToken", this.token)
 
       let body = new FormData()
       body.append("id", id)
@@ -175,13 +216,13 @@ const app = new Vue({
 
     updateEntry: function(e) {
       const id = e.target.parentElement.id
-      const amount = e.target[2].value ? e.target[2].value : 1
+      const amount = e.target[1].value ? e.target[1].value : 1
 
       let headers = new Headers()
-      headers.append("X-CSRFToken", e.target[0].value)
+      headers.append("X-CSRFToken", this.token)
 
       let body = new FormData()
-      body.append("text", e.target[1].value)
+      body.append("text", e.target[0].value)
       body.append("amount", amount)
       body.append("id", id)
 
@@ -195,18 +236,75 @@ const app = new Vue({
         this.lists = data
       })
     }
+  },
 
-  }
+  template: `
+    <div>
+      <form action="new_list/" method="post" v-on:submit.prevent="newList($event)">
+        <input type="text" name="title" placeholder="New list">
+        <input type="submit" value="Add">
+      </form>
+
+      <template v-if="lists !== undefined">
+        <div v-for="list in lists" :id="list.id">
+
+          <hr>
+
+          <!--
+          <p>[[ list.title ]]</p>
+          <p>id: [[ list.id ]]</p>
+          <p>Archived: [[ list.archived ]]</p>
+          <p>Collapsed: [[ list.collapsed ]]</p>
+          -->
+
+          <form action="" method="post" v-on:submit.prevent="updateList($event)">
+            <input type="text" name="title" :value="[[ list.title ]]">
+            <input type="submit" value="Save">
+          </form>
+
+          <button v-on:click="archiveList($event)">Archive</button>
+          <button v-on:click="collapseList($event)">Collapse</button>
+
+          <ul style="padding: 0">
+            <template v-for="entry in list.entries">
+              <template v-if="entry.header_id === list.id">
+                <li :id="entry.id">
+
+                  <!--
+                  <p>[[ entry.amount ]] [[ entry.text ]] - Checked: [[ entry.done ]]</p>
+                  <p>id: [[ entry.id ]]</p>
+                  -->
+
+                  <form action="" method="post" v-on:submit.prevent="updateEntry($event)">
+                    <input type="text" name="text" :value="[[ entry.text ]]">
+                    <input type="number" name="amount" :value="[[ entry.amount ]]">
+                    <input type="submit" value="Save">
+                  </form>
+
+                  <button v-on:click="deleteEntry($event)">Delete</button>
+                  <button v-on:click="checkEntry($event)">Check</button>
+                </li>
+              </template>
+            </template>
+          </ul>
+
+          <form action="" method="post" v-on:submit.prevent="newEntry($event)">
+            <input type="text" name="text" placeholder="New item">
+            <input type="number" name="amount" placeholder="Amount">
+            <input type="submit" value="Add">
+          </form>
+
+        </div>
+
+      </template>
+    </div>
+  `
 })
 
-app.getData()
-
-/*
-id
-title
-archived
-collapsed
-*/
+new Vue({
+  delimiters: ["[[", "]]"],
+  el: "#app"
+})
 
 /*
 "1": {
