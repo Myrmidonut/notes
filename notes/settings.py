ac86d994f1eba3c4ff4e171bee6bc7ac033db278
Frederik Funke <frederik-funke@gmx.de>
2019-03-19 21:47:33 +0100

    env variables, postgresql as db

--- notes/settings.py
+++ notes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_^$#)upmjer0&hbicpl#rz%2oix408v&-ho5pf(0j)z_11@k^-'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +78,15 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 
 DATABASES = {
   'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #'ENGINE': 'django.db.backends.sqlite3',
+    #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+    "ENGINE": config("ENGINE"),
+    "NAME": config("NAME"),
+    "USER": config("NAME"),
+    "PASSWORD": config("PASSWORD"),
+    "HOST": config("HOST"),
+    "PORT": config("PORT")
   }
 }
 
